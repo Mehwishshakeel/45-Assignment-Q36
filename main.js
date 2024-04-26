dd925b3dbdf4ec2033af6a7e359a19ae1adfd212
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-04-26 12:53:26 +0500

    Function

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function make_shirt(size, printmessage) {
+    console.log(`You selected ${size} size with ${printmessage} print on shirt`);
+}
+make_shirt("medium", "GenAI");
+make_shirt("small", "happy");
---
+++ main.ts
@@ -0,0 +1,5 @@
+function make_shirt(size:string, printmessage:string){
+  console.log(`You selected ${size} size with ${printmessage} print on shirt`);
+}
+make_shirt( "medium" ,"GenAI" );
+make_shirt("small","happy");
